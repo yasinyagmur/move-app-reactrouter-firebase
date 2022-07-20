e7a42c8d0b7cb54317af67efce12888e857c3107
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-20 13:02:22 +0300

    v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Container } from "@mui/system";
+import MovieCards from "../components/Cards/MovieCards";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+    <Container sx={{ display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+          mt: 3,
+        }}
+      >
+        <TextField fullWidth label="Search Movie" id="fullWidth" />
+      </Box>
+    </Container>
+    <MovieCards/>
+    </>
+  );
+};
 
-export default Home
+export default Home;
