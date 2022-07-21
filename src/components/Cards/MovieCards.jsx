217b1665c59bac2d52a03db41e463760ebce6750
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-21 15:35:11 +0300

    movie cards created

--- src/components/Cards/MovieCards.jsx
+++ src/components/Cards/MovieCards.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import "./MovieCards.css";
+import { Box } from "@mui/system";
+import { AuthContext } from "../../context/AuthContext";
 
-const MovieCards = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+export default function MovieCards({
+  poster_path,
+  title,
+  overview,
+  vote_average,
+  id,
+}) {
+  const { currentUser } = React.useContext(AuthContext);
+
   return (
-    <div>MovieCards</div>
-  )
-}
+    <Box className="Cards">
+      <Card sx={{ maxWidth: 345, margin: "auto" }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="500"
+            image={poster_path ? IMG_API + poster_path : defaultImage}
+            alt={title}
+          />
+          <CardContent>
+            <Typography
+              sx={{ height: "2rem" }}
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+              {title}
+            </Typography>
 
-export default MovieCards
+            <Typography
+              className="overview"
+              variant="body1"
+              color="text.primary"
+              classes="overview"
+            >
+              {overview}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small">Movie Detail</Button>
+          {currentUser ? (
+            <Typography
+              variant="h4"
+              color="primary"
+              sx={{ marginLeft: "auto" }}
+            >
+              {vote_average}
+            </Typography>
+          ) : (
+            <Typography variant="p" color="primary" sx={{ marginLeft: "auto" }}>
+              Movie Point -
+            </Typography>
+          )}
+        </CardActions>
+      </Card>
+    </Box>
+  );
+}
+// onClick={()=>getMovieDetails(id,navigate,title)}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Container } from "@mui/system";
 import MovieCards from "../components/Cards/MovieCards";
+import axios from "axios";
+import { ImageListItem } from "@mui/material";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    setLoading(true);
+    axios
+      .get(API)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err))
+      .finally(setLoading(false));
+  };
   return (
     <>
-    <Container sx={{ display: "flex", justifyContent: "center" }}>
-      <Box
-        sx={{
-          width: 500,
-          maxWidth: "100%",
-          mt: 3,
-        }}
-      >
-        <TextField fullWidth label="Search Movie" id="fullWidth" />
-      </Box>
-    </Container>
-    <MovieCards/>
+      <Container sx={{ display: "flex", justifyContent: "center" }}>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+            mt: 3,
+          }}
+        >
+          <TextField fullWidth label="Search Movie" id="fullWidth" />
+        </Box>
+      </Container>
+
+      {loading ? (
+        <ImageListItem>
+          <img src="../assets/loading.gif" alt="loadinggif" loading="lazy" />
+        </ImageListItem>
+      ) : (
+        movies?.map((movie) => <MovieCards key={movie.id} {...movie} />)
+      )}
     </>
   );
 };
