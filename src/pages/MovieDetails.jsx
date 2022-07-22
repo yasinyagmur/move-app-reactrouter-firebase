76b02cbcdf7761133bbbc681a98bf1c7a4277cf3
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-22 16:50:48 +0300

    fixed

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,7 +6,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea, Container } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 import VideoSection from "../components/Video/VideoSection";
 
 const MovieDetails = () => {
